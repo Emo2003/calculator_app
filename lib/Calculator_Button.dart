4cb32a5127f7cba9eb92ef88c94a60c31b91d4c2
Emo2003 <mdhta477@gmail.com>
2025-08-31 03:19:28 +0300

    " modified the delete button's action.(using Function OnDeleteDigit)"

--- lib/Calculator_Button.dart
+++ lib/Calculator_Button.dart
@@ -2,48 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CalculatorButton extends StatelessWidget {
- final String title;
- final Color backgroundColor;
+  final String title;
+  final Color backgroundColor;
   final Color textColor;
   final int flex;
   final bool isImage;
-  final  Function(String) onTap;
-  const CalculatorButton({super.key , required this.title ,
+  final void Function(String )onTap;
+  const CalculatorButton({super.key , required this.title,
     this.backgroundColor= const Color(0xff303136),
-    this .textColor=const Color(0xff29A8FF ),
-    this.flex=1  , this.isImage=false,
-    required this.onTap
-  });
+    this.textColor= const Color(0xff29A8FF),
+    this.flex=1 , required this.onTap , this.isImage=false});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return  Expanded(
       flex: flex,
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(8.0),
         child: Material(
+          borderRadius: BorderRadius.circular(16),
           color: backgroundColor,
-          borderRadius:  BorderRadius.circular(30),
           child: InkResponse(
-          containedInkWell: true,
-            borderRadius: BorderRadius.circular(30),
-            onTap: (){onTap(title);},
+            onTap: (){
+              onTap(title);
+            },
+            containedInkWell: true,
+            borderRadius: BorderRadius.circular(16),
             child: Container(
-              height: double.infinity,
               margin: EdgeInsets.symmetric(
                   vertical: 11,
                   horizontal: 10
               ),
+
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16)
+                borderRadius: BorderRadius.circular(16),
               ),
-              child: isImage?
-              Center(child: SvgPicture.asset("assets/images/delete.svg", height: 30,width: 30, fit: BoxFit.cover,)):
-              Center(
-                child: Text(title , style: TextStyle(fontSize:30 ,
-                    fontWeight: FontWeight.w600,color: textColor ),
-                  ),
-              ),),
+              child: isImage? Center(child: SvgPicture.asset("assets/images/delete.svg" ))
+                  :Center(
+                child: Text(title ,
+                  style: TextStyle(fontSize:27,
+                      fontWeight: FontWeight.w600  , color: textColor),),
+              ),
+            ),
           ),
         ),
       ),
